aea18d471706ca0ea726eae5d21f1f29d17d0e7a
RobinRMC <github@robinmail.nl>
2024-03-19 20:11:40 +0100

    Update Utils.java

--- src/main/java/nl/rmcservers/birthdays/Utils.java
+++ src/main/java/nl/rmcservers/birthdays/Utils.java
@@ -26,4 +26,45 @@ public class Utils {
         SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd");
         return dateFormat.format(date);
     }
+}package nl.rmcservers.birthdays;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Utils {
+    public static JSONObject loadJSONFromFile(File file) {
+        JSONParser parser = new JSONParser();
+        try (FileReader reader = new FileReader(file)) {
+            return (JSONObject) parser.parse(reader);
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getCurrentDate() {
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd");
+        return dateFormat.format(date);
+    }
+    
+    public static UUID getPlayerUUID(String playerName) {
+        Player player = Bukkit.getPlayer(playerName);
+        if (player != null) {
+            return player.getUniqueId();
+        } else {
+            return null;
+        }
+    }
 }
