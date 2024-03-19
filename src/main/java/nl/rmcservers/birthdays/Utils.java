f7bc82887936b8cc73015c0727ea1568fb9f70d4
RobinRMC <github@robinmail.nl>
2024-03-19 20:12:35 +0100

    Fix Utils.java

--- src/main/java/nl/rmcservers/birthdays/Utils.java
+++ src/main/java/nl/rmcservers/birthdays/Utils.java
@@ -1,33 +1,5 @@
 package nl.rmcservers.birthdays;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-public class Utils {
-    public static JSONObject loadJSONFromFile(File file) {
-        JSONParser parser = new JSONParser();
-        try (FileReader reader = new FileReader(file)) {
-            return (JSONObject) parser.parse(reader);
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static String getCurrentDate() {
-        Date date = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd");
-        return dateFormat.format(date);
-    }
-}package nl.rmcservers.birthdays;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
