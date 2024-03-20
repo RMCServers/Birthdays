c19a57df9389887429b870e271d64800baaad049
RobinRMC <github@robinmail.nl>
2024-03-20 20:57:01 +0100

    Import OfflinePlayer class

--- src/main/java/nl/rmcservers/birthdays/Birthdays.java
+++ src/main/java/nl/rmcservers/birthdays/Birthdays.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.OfflinePlayer;
 import co.aikar.taskchain.BukkitTaskChainFactory;
 import co.aikar.taskchain.TaskChainFactory;
 import co.aikar.taskchain.TaskChain;
