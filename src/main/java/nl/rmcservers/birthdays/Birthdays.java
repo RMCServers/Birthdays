56a57f754da3a0ddf1f3ecadd5e999c28d26bc67
RobinRMC <github@robinmail.nl>
2024-03-21 22:50:16 +0100

    Import Bukkit

--- src/main/java/nl/rmcservers/birthdays/Birthdays.java
+++ src/main/java/nl/rmcservers/birthdays/Birthdays.java
@@ -1,5 +1,6 @@
 package nl.rmcservers.birthdays;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
