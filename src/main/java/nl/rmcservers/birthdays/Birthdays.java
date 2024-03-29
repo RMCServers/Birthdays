2b25ba1f3476e03b3243a5bf7b128d26c80e5c58
RobinRMC <github@robinmail.nl>
2024-03-30 00:39:46 +0100

    Fix

--- src/main/java/nl/rmcservers/birthdays/Birthdays.java
+++ src/main/java/nl/rmcservers/birthdays/Birthdays.java
@@ -310,7 +310,7 @@ public class Birthdays extends JavaPlugin implements CommandExecutor, TabComplet
 
         // Sort the list alphabetically
         getLogger().info("Sorting players...");
-        Arrays.sort(playerList, String.CASE_INSENSITIVE_ORDER);
+        Arrays.sort(playerArray, String.CASE_INSENSITIVE_ORDER);
         getLogger().info("Players sorted.");
 
         // Convert the sorted array back to ArrayList
